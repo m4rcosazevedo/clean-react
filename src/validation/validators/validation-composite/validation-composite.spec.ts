d6c385fc8c92127b68eb2e8f2aa364fc2000cdf8
Marcos Azevedo <marcos.azevedo@izap.com.br>
2020-08-14 17:06:59 -0300

    test: ensure ValidationComposite return error if any validation fails

---
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -0,0 +1,17 @@
+import { ValidationComposite } from '@/validation/validators/validation-composite/validation-composite'
+import { FieldValidationSpy } from '@/validation/validators/test/mock-field-validation'
+
+describe('ValidationComposite', () => {
+  test('Should return error if any validation fails', () => {
+    const fieldValidationSpy = new FieldValidationSpy('any_field')
+    fieldValidationSpy.error = Error('first_error_message')
+    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
+    fieldValidationSpy2.error = Error('second_error_message')
+    const sut = new ValidationComposite([
+      fieldValidationSpy,
+      fieldValidationSpy2
+    ])
+    const error = sut.validate('any_field', 'any_value')
+    expect(error).toBe('first_error_message')
+  })
+})
